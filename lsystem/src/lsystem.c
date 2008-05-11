ab58ae9f1e98f6e995769b935e765ae9774407c2
cobranail <cobranail@54947efe-cf45-0410-b57e-01d4dc9673c2>
2008-05-11 05:48:53 +0000

    add self-sensitive (need test)

--- lsystem/src/lsystem.c
+++ lsystem/src/lsystem.c
@@ -55,6 +55,56 @@ float min(float a, float b){
 	else return b;
 }
 
+float distancepp(float x1, float y1, float x2, float y2){
+	
+	return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+	
+}
+
+
+int intersect(Seg *np1, Seg *np2, float poi[2]){
+	
+	float k1,k2,b1,b2,x11,x12,y11,y12,x21,x22,y21,y22;
+	
+	x11=np1->spx;
+	x12=np1->epx;
+	x21=np2->spx;
+	x22=np2->epx;
+	
+	y11=np1->spy;
+	y12=np1->epy;
+	y21=np2->spy;
+	y22=np2->epy;
+	
+	k1=(y11-y12)/(x11-x12);
+	b1=y11-x11*k1;
+	
+	k2=(y21-y22)/(x21-x22);
+	b2=y21-x21*k2;
+	
+	if(fabs(k1-k2)>0.01){
+		
+		poi[0]=(b2-b1)/(k1-k2); //求交点
+		poi[1]=k1*poi[0]+b1;
+		
+		if( poi[0]<max(x11,x12) && poi[0]>min(x11,x12) && poi[0]<max(x21,x22) && poi[0]>min(x21,x22)  && (distancepp(x11,y11,poi[0],poi[1])>0.001) && (distancepp(x12,y12,poi[0],poi[1])>0.001)){
+			
+			
+			//printf("L:(%4f,%4f) (%4f,%4f),  L:(%4f,%4f) (%4f,%4f),  intersect: (%4f,%4f)\n", x11,y11,x12,y12,x21,y21,x22,y22,poi[0],poi[1]);
+			
+			
+			return 1;
+		}
+		
+		
+	}
+	
+	return 0;
+	
+}
+
+
+
 void drawPixel(char *buffer, int w, int h, int x, int y, char rgb[]){
 	
 	//image bound check
@@ -92,7 +142,7 @@ void drawLine(char *buffer, int w, int h, float sp[], float ep[], char rgb[]){
 		}
 	}
 	else {
-		printf("%f,%f\n",ep[0]-sp[0],ep[1]-sp[1]);
+		//printf("%f,%f\n",ep[0]-sp[0],ep[1]-sp[1]);
 		
 	}
 
@@ -103,10 +153,13 @@ void drawFigure(char *raster, int width, int height, char *str){
 	char *p;
 	p=str;
 	float step = 1;
-	float da = M_PI_2/3.0;
+	float da = M_PI_2,dda=0;;
 	float a[2]={5,5};
 	float b[2]={500,500};
 	float sp[2],ep[2];
+	float poi[2];
+	int intersection;
+	
 	char rgb[3];
   	rgb[0]=0;
 	rgb[1]=255;
@@ -136,7 +189,7 @@ void drawFigure(char *raster, int width, int height, char *str){
      state.agl=0;
      
      
-     Seg * np;
+     Seg *np, *nptmp, *npnew, *np2;
      ARState * asp;
      
      SLIST_INIT(&line); 		     /* Initialize the list. */
@@ -144,39 +197,80 @@ void drawFigure(char *raster, int width, int height, char *str){
      
      while (*p!='\0') {
     	 
-    	 if(*p=='F'){
-    		 
-		     np = malloc(sizeof(struct seg));	/* Insert at the head. */
-		     np->spx=state.x;
-		     np->spy=state.y;
-		     
-		     state.x+=step*cos(state.agl);
-		     state.y+=step*sin(state.agl);
-		     
-     
-		     np->epx=state.x;
-		     np->epy=state.y;
-		     
-		     
-		     np->drawable=1;
-		     
-		     bnd.xmax=max( max(bnd.xmax, np->spx), np->epx );
-		     bnd.xmin=min( min(bnd.xmin, np->spx), np->epx );
-		     bnd.ymax=max( max(bnd.ymax, np->spy), np->epy );
-		     bnd.ymin=min( min(bnd.ymin, np->spy), np->epy );
-		     
-		     {SLIST_INSERT_HEAD(&line, np, entries);}
-		     
-		     i++;
-		  
-    		 
-    	 }
+    	 if (*p=='F') {
+
+			np = malloc(sizeof(struct seg)); /* Insert at the head. */
+			np->spx=state.x;
+			np->spy=state.y;
+
+			state.x+=step*cos(state.agl);
+			state.y+=step*sin(state.agl);
+
+			np->epx=state.x;
+			np->epy=state.y;
+
+			np->drawable=1;
+
+			SLIST_FOREACH(nptmp, &line, entries) {
+				//intersection=intersect(np, nptmp, poi);
+				if(intersect(np, nptmp, poi)==1) { //如果相交
+
+
+					np->epx=poi[0];
+					np->epy=poi[1];
+
+					state.x=poi[0];
+					state.y=poi[1];
+
+					if( (distancepp(nptmp->spx,nptmp->spy,poi[0],poi[1])>0.001) && (distancepp(nptmp->epx,nptmp->epy,poi[0],poi[1])>0.001) ) {
+
+						npnew= malloc(sizeof(struct seg));
+						npnew->spx=poi[0];
+						npnew->spy=poi[1];
+						npnew->epx=nptmp->epx;
+						npnew->epy=nptmp->epy;
+						npnew->drawable=1;
+
+						nptmp->epx=poi[0];
+						nptmp->epy=poi[1];
+						
+						
+						{	SLIST_INSERT_AFTER(nptmp, npnew, entries);}
+						i++;
+
+					}
+
+				} 
+
+			}
+
+			bnd.xmax=max(max(bnd.xmax, np->spx), np->epx);
+			bnd.xmin=min(min(bnd.xmin, np->spx), np->epx);
+			bnd.ymax=max(max(bnd.ymax, np->spy), np->epy);
+			bnd.ymin=min(min(bnd.ymin, np->spy), np->epy);
+
+			if ( distancepp(np->spx,np->spy,np->epx, np->epy)>0.5 ) {
+				{
+					//puts("add np");
+					
+					SLIST_INSERT_HEAD(&line, np, entries);
+					
+					
+				}
+				
+				i++;
+			}
+			
+
+		}
     	 
     	 else if (*p=='f') {
     		 
 		     np = malloc(sizeof(struct seg));	/* Insert at the head. */
 		     np->spx=state.x;
 		     np->spy=state.y;
+		     
+		     
 		     
 		     state.x+=step*cos(state.agl);
 		     state.y+=step*sin(state.agl);
@@ -196,10 +290,12 @@ void drawFigure(char *raster, int width, int height, char *str){
     	 
     	 else if (*p=='+') {
     		 
-    		 state.agl+=(da+(float)((random () % 200)-50));
+    		 state.agl+=(da+dda);//(da+(float)((random () % 100)-0)/100.0);
+
     	 }
     	 else if (*p=='-') {
-    		 state.agl-=(da+(float)((random () % 200)-50));
+    		 state.agl-=(da+dda);//(da+(float)((random () % 100)-0)/100.0);
+
     	 }
     	 else if (*p=='[') {
     		 asp = malloc(sizeof(struct arstate));
@@ -342,9 +438,9 @@ char * replaceString(char *str1, char *str2, char *str){
 
 int main(int argc, char *argv[]) {
 	
-	char *str="FF";
+	char *str="F+F+F+F";
 	char *rule1='F';
-	char *rule2="F[+F]+F[-F]F";
+	char *rule2="F[+F][--F]F";
 	char *outstr;
 	char *tmp;
 	
