553efb773c92d2849b8dbc206ba5dd2167ac94c8
cobranail <cobranail@54947efe-cf45-0410-b57e-01d4dc9673c2>
2009-05-30 15:41:15 +0000

    CGAparser can generate command tree correctly now, not lose cmd anymore. tester also updated for a better output for checking.

--- CityEngineLite/testCGAparsing.py
+++ CityEngineLite/testCGAparsing.py
@@ -1,7 +1,25 @@
 from CGAparser import *
-
+import types
 f = open(sys.argv[1],'r')
 text=f.readlines()
+ws=''
+cmd=[]
+def nt(ws,cmd):
+	print ws,
+	if type(cmd) is types.ListType:
+		for x in cmd:
+			if type(x) is types.ListType:
+				print '\n'
+				nt(ws+'    ',x)
+			else:
+				print x,
+		#print '\n'
+	else:
+		print cmd
+
 for str in text:
 	cmd=parsingNotation(str)
-	print cmd
+	if len(cmd)>0:
+		print cmd,'\n'
+		nt(ws,cmd)
+		print '\n'
